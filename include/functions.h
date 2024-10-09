caa94b2c3d73a138e23ebaf30fbea99e667a25cb
Jeo0 <jjrelb@gmail.com>
2024-10-09 23:19:53 +0800

    pseudo 2.0

--- include/functions.h
+++ include/functions.h
@@ -1,3 +1,16 @@
 
 float instructions[8] = {};
 float current_execute[8] = {};
+
+const long interval = 1000;
+int ledState = LOW;
+
+
+
+  enum State {
+    White,
+    Rainbow,
+    Continuous
+  };
+  State state = White;
+
--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,19 @@ void loop() {
     start timer
     instruction.append(timer)
   
-  if another input && timer > 100 ms:   // once valid time, another input is good
+  if another input && timer > 100 ms && index != 7:   // once valid time, another input is good
     instruction.append(timer)           // store instructions[index]
     index++
     timer = 0
 
+  
+  // after 1000 ms:
+  // reset index and timer 
+  // parse the instruction, 
+
+  // if okay:
+  // current_instruction = instruction
+  // execute current_instruction
   
   if timer > 1000 ms:
     index = 0
@@ -49,29 +57,48 @@ void loop() {
 
   // using enums as state,
   switch(state){
-
+    case White:
+      output(pin, LOW);
+    case Rainbow:
+      output(pin, HIGH);
+    case Continuous:
+      handleContinuous(timer);
   }
 
   
+  enum State {
+    White
+    Rainbow
+    Continuous
+  };
   void ParseThis(float* instructions){
 
+
+    // if valid; update the state accordingly
+    state = ...
   }
-    
 
 
+  void HandleContinuous(float* timer){
+    // code from "https://docs.arduino.cc/built-in-examples/digital/BlinkWithoutDelay/"
 
+    unsigned long currentMillis = *timer; 
+    // interval = 1000 ms
 
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
 
-  // after 1000 ms:
-  // reset index and timer 
-  // parse the instruction, 
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
 
-  // if okay:
-  // current_instruction = instruction
-  // execute current_instruction
-  
-    
-
+      // set the LED with the ledState of the variable:
+      digitalWrite(ledPin, ledState);
+  }
   
   */
   
